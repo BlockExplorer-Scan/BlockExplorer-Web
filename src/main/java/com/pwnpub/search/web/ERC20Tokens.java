a323e09c90bc8749cb1d176e4c76a358e01153ae
soobeencn <soobeencn@gmail.com>
2019-01-28 16:21:11 +0800

    ERC20 获取totalSupply

--- src/main/java/com/pwnpub/search/web/ERC20Tokens.java
+++ src/main/java/com/pwnpub/search/web/ERC20Tokens.java
@@ -1,6 +1,7 @@
 package com.pwnpub.search.web;
 
 import com.pwnpub.search.config.CoinName;
+import com.pwnpub.search.utils.CommonUtils;
 import com.pwnpub.search.utils.ResponseResult;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
@@ -15,8 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -46,7 +51,6 @@ public class ERC20Tokens {
 
         if (contractAddress != null) {
 
-
             BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
 
             boolQueryBuilder.must(QueryBuilders.matchQuery("status", coinName.getErc20()));
@@ -65,10 +69,6 @@ public class ERC20Tokens {
 
             for (SearchHit hit : searchResponse.getHits()) {
 
-                Object transactionHash = hit.getSourceAsMap().get("transactionHash");
-                //web3  获取quantity
-
-
                 hit.getSourceAsMap().put("statusName", coinName.getErc20Name());
 
                 list.add(hit.getSourceAsMap());
@@ -77,5 +77,49 @@ public class ERC20Tokens {
         }
         return ResponseResult.build(200, "query erc20 transfer datas success", list);
     }
+
+    @GetMapping("/queryERC20TokenCounts")
+    public ResponseResult queryERC20TokenCounts(
+            @RequestParam(name = "contractAddress", required = true )String contractAddress
+
+    ){
+        Map<String, Object> map = new HashMap<>();
+
+        if (contractAddress != null) {
+
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+            boolQueryBuilder.must(QueryBuilders.matchQuery("status", coinName.getErc20()));
+            boolQueryBuilder.must(QueryBuilders.matchQuery("address", contractAddress));
+
+
+            SearchRequestBuilder searchRequestBuilder = this.client.prepareSearch("erc20")
+                    .setTypes("data")
+                    .setSearchType(SearchType.QUERY_THEN_FETCH)
+                    .addSort("blockNumber", SortOrder.DESC)
+                    .setQuery(boolQueryBuilder);
+
+            SearchResponse searchResponse = searchRequestBuilder.get();
+
+            long totalHits = searchResponse.getHits().getTotalHits();
+            map.put("Transfers", totalHits);
+
+            Web3j web3 = null;
+            try {
+                web3 = Web3j.build(new HttpService("http://n8.ledx.xyz"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ResponseResult.build(201, "call web3j failed...");
+            }
+
+            BigInteger tokenTotalSupply = CommonUtils.getTokenTotalSupply(web3, contractAddress);
+
+            map.put("TokenTotalSupply", tokenTotalSupply);
+
+
+        }
+        return ResponseResult.build(200, "query erc20 transfer datas success", map);
+    }
+
 
 }
