058f62991b4a3d8dd199e649e6eabe295a206b4c
soobeencn <soobeencn@gmail.com>
2019-06-24 11:10:54 +0800

    新增:Token钱包

---
+++ src/main/java/com/pwnpub/search/web/TokenController.java
@@ -0,0 +1,16 @@
+package com.pwnpub.search.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author soobeenwong
+ * @date 2019-06-24 11:08
+ * @desc 钱包管理类
+ */
+
+@RestController
+@RequestMapping("/Token")
+public class TokenController {
+
+}
